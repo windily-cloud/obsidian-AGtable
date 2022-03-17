57f0f268fba7f542446a43b39e0daa2338064a9d
windilycloud <wwww.4399li@qq.com>
2022-03-17 13:03:35 +0800

    dev: init project

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss'
+
+export default {
+  input: './src/main.ts',
+  output: {
+    dir: '.',
+    sourcemap: 'inline',
+    sourcemapExcludeSources: isProd,
+    format: 'cjs',
+    exports: 'default',
+  },
+  external: ['obsidian'],
+  plugins: [
+    typescript(),
+    nodeResolve({browser: true}),
+    commonjs(),
+    postcss({
+      extract: true,
+      extract: 'styles.css'
+    })
+  ]
+};
