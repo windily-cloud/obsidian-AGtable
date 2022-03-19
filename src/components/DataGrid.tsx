5d1738fbf51ec046d16f830e15521287ce503a57
windilycloud <wwww.4399li@qq.com>
2022-03-19 23:38:14 +0800

    dev: 重构tableEdior

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -1,4 +1,4 @@
-import { App } from 'obsidian'
+import { App, editorViewField } from 'obsidian'
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/dist/styles/ag-grid.css'
@@ -20,6 +20,7 @@ import {
   DragStoppedEvent,
   RowDragEvent,
 } from 'ag-grid-community'
+import TableEditor from 'tableEditor'
 
 interface Props {
   app: App
@@ -35,6 +36,9 @@ interface DataGridTable {
 export default function DataGrid(props: Props) {
   //theme init
   const isDarkMode = Array.from(document.body.classList).includes('theme-dark')
+
+  //editor init
+  const tableEditor = new TableEditor(props.app, props.tableId)
   //data init
   if (!markdownTableToDataGrid(props.tableString)) {
     console.log('parse markdwon table failed!')
@@ -61,79 +65,60 @@ export default function DataGrid(props: Props) {
         tableId: props.tableId,
       },
     }),
-    []
+    [props.tableId]
   )
 
   //table menu
-  const [visible, setVisible] = useState({ visible: false })
+  const [visible, setVisible] = useState(false)
+  const [rowChangeIndex, setRowChangeIndex] = useState(undefined)
 
   function TableMenu() {
     const menuRef = useRef(null)
+    console.log('variable in table menu:', rowData)
     useEffect(() => {
       document.addEventListener('click', () => {
-        if (visible.visible) {
+        if (visible) {
           document.addEventListener('click', () => {
             menuRef.current.style.display = 'none'
           })
-          setVisible({ visible: false })
+          setVisible(false)
         }
       })
     })
 
-    async function handleAddRowBelow() {
-      const fileContent = await props.app.vault.cachedRead(
-        props.app.workspace.getActiveFile()
+    function handleAddRowBelow() {
+      console.log('onAddRow start:', columnDefs, rowData)
+      const { row: newRow } = tableEditor.addRowBelow(
+        {
+          column: columnDefs,
+          row: rowData,
+        },
+        rowChangeIndex
       )
-      const tableReg = new RegExp(
-        `(?<=tableId:\\s${props.tableId}\\s)[\\w\\W]*(?=\\W+\`\`\`)`
-      )
-      const oldTableString = fileContent.match(tableReg)[0]
-      const { column: columnDefs, row: rowData } = markdownTableToDataGrid(
-        oldTableString
-      ) as DataGridTable
-
-      const newRowList = columnDefs.map((el: { [key: string]: string }) => {
-        return {
-          [el.field]: '',
-        }
-      })
-      console.log('addrow:', columnDefs, rowData)
-      const newRow = Object.assign({}, ...newRowList)
-      const rowIndex = localStorage.getItem('agTableRowIndex')
-      rowData.splice(parseInt(rowIndex) + 1, 0, newRow)
-      const tableString = dataGridToMarkdownTable({
-        column: columnDefs,
-        row: rowData,
-      })
-      replaceTable(props.app, props.tableId, tableString)
-      setVisible({ visible: false })
+      setRowData(newRow)
+      setVisible(false)
     }
 
-    async function handleDeleteThisRow() {
-      const fileContent = await props.app.vault.cachedRead(
-        props.app.workspace.getActiveFile()
-      )
-      const tableReg = new RegExp(
-        `(?<=tableId:\\s${props.tableId}\\s)[\\w\\W]*(?=\\W+\`\`\`)`
-      )
-      const oldTableString = fileContent.match(tableReg)[0]
-      const { column: columnDefs, row: rowData } = markdownTableToDataGrid(
-        oldTableString
-      ) as DataGridTable
+    function handleDeleteThisRow() {
       const rowIndex = localStorage.getItem('agTableRowIndex')
-      rowData.splice(parseInt(rowIndex), 1)
-      const tableString = dataGridToMarkdownTable({
-        column: columnDefs,
-        row: rowData,
-      })
+      console.log('read rowIndex', rowIndex)
+      if (rowIndex === undefined) {
+        return
+      }
+      const { row: newRow } = tableEditor.deleteThisRow(
+        {
+          column: columnDefs,
+          row: rowData,
+        },
+        rowIndex
+      )
 
-      setRowData(rowData)
-      replaceTable(props.app, props.tableId, tableString)
-      setVisible({ visible: false })
+      setRowData(newRow)
+      setVisible(false)
     }
 
     return (
-      visible.visible && (
+      visible && (
         <div id="table-menu" ref={menuRef}>
           <div onClick={handleAddRowBelow}>{t('addRowBelow')}</div>
           <div onClick={handleDeleteThisRow}>{t('deleteThisRow')}</div>
@@ -145,6 +130,8 @@ export default function DataGrid(props: Props) {
   function handleContextMenu(e: any) {
     //console.log(e)
     localStorage.setItem('agTableRowIndex', e.rowIndex)
+    localStorage.setItem('agTableChangeRow', JSON.stringify(rowData))
+    console.log('set rowIndex:', e.rowIndex)
     let rightclick = document.getElementById('table-menu')
     let clientx = e.event.pageX
     let clienty = e.event.pageY
@@ -158,128 +145,55 @@ export default function DataGrid(props: Props) {
       const menuDiv = document.createElement('div')
       document.body.appendChild(menuDiv)
       ReactDOM.render(React.createElement(TableMenu), menuDiv)
-      setVisible({ visible: true })
+      setVisible(true)
     }
   })
 
   //cell edit setting
   function onCellEditingStopped(event: CellEditingStoppedEvent) {
-    console.log(event)
     const newData = event.data
     const rowIndex = event.rowIndex
 
-    let newRow = rowData.map((el, index) => {
-      if (index === rowIndex) {
-        return newData
-      } else {
-        return el
-      }
-    })
-
-    const tableString = dataGridToMarkdownTable({
-      column: column,
-      row: newRow,
-    })
-    replaceTable(props.app, props.tableId, tableString)
+    tableEditor.changeCellValue(
+      { column: columnDefs, row: rowData },
+      newData,
+      rowIndex
+    )
   }
 
   //column drag
   function onColumnMoved(event: ColumnMovedEvent) {
-    //console.log(event.api)
     const colId = event.column.getColId()
     const toIndex = event.toIndex
-    //console.log('start drag', colId, toIndex)
-    const newColumn = column.filter((el: ColDef) => {
-      return el.field != colId
-    })
-
-    newColumn.splice(toIndex, 0, { field: colId })
-    //console.log(newColumn)
-
-    //console.log('process row:', rowData)
-    const newRow: Array<{ [key: string]: string }> = []
-    //console.log(rowData)
-    rowData.map((rowItem: { [index: string]: string }) => {
-      const rowList = Object.entries(rowItem)
-      //console.log('rowList:', rowList)
-      let fromIndex: number
-      rowList.some((el, index) => {
-        if (el[0] === colId) {
-          fromIndex = index
-          return true
-        }
-      })
-      //console.log(fromIndex, toIndex)
-      if (toIndex < fromIndex) {
-        const deletedItem = rowList.splice(fromIndex, 1)
-        //console.log('deleteItem:', deletedItem)
-        rowList.splice(toIndex, 0, deletedItem[0])
-        //console.log(rowList)
-      } else {
-        rowList.splice(toIndex + 1, 0, rowList[fromIndex])
-        rowList.splice(fromIndex, 1)
-      }
-      const rowObj = rowList.map((el: string[]) => {
-        return {
-          [el[0]]: el[1],
-        }
-      })
-      const row = Object.assign({}, ...rowObj)
-      //console.log('row:', row)
-      newRow.push(row)
-      //console.log(newRow)
-    })
-
-    const tableString = dataGridToMarkdownTable({
-      column: newColumn,
-      row: newRow,
-    })
-
-    localStorage.setItem('agTabelMovedString', tableString)
+    const { column: newColumn, row: newRow } = tableEditor.dragColumn(
+      { column: columnDefs, row: rowData },
+      colId,
+      toIndex
+    )
     setColumnDefs(newColumn)
     setRowData(newRow)
   }
 
-  function onDragStopped(event: DragStoppedEvent) {
-    console.log('drogColumn:', columnDefs, rowData)
-    const tableString = localStorage.getItem('agTabelMovedString')
-    replaceTable(props.app, props.tableId, tableString)
+  function onDragStopped() {
+    tableEditor.replaceMdFileTable({ column: columnDefs, row: rowData })
   }
 
   //row drag
   function onRowDragEnd(event: RowDragEvent) {
     console.log(event)
-    const toRowData = event.node.data
-    let newRow = rowData
+    const srcRowData = event.node.data
     const toIndex = event.overIndex
 
-    let fromIndex: number
-    newRow.some((el, index) => {
-      console.log(el, toRowData)
-      if (isObjShallowEqual(el, toRowData)) {
-        fromIndex = index
-        return true
-      }
-    })
-    if (fromIndex === undefined) {
-      throw new Error('not found fromIndex!')
-    }
-
-    if (toIndex < fromIndex) {
-      const deletedItem = newRow.splice(fromIndex, 1)
-      newRow.splice(toIndex, 0, deletedItem[0])
-    } else {
-      newRow.splice(toIndex + 1, 0, newRow[fromIndex])
-      newRow.splice(fromIndex, 1)
-    }
-
-    const tableString = dataGridToMarkdownTable({
-      column: column,
-      row: newRow,
-    })
+    const { row: newRow } = tableEditor.dragRow(
+      {
+        column: columnDefs,
+        row: rowData,
+      },
+      srcRowData,
+      toIndex
+    )
 
     setRowData(newRow)
-    localStorage.setItem('agTabelMovedString', tableString)
   }
 
   return (
---
+++ src/tableEditor.ts
@@ -0,0 +1,253 @@
+import { ColDef } from 'ag-grid-community'
+import { App } from 'obsidian'
+
+interface DataGridTable {
+  column: ColDef[]
+  row: Array<{ [index: string]: string }>
+}
+
+export default class TableEditor {
+  app: App
+  tableId: string
+
+  constructor(app: App, tableId: string) {
+    this.app = app
+    this.tableId = tableId
+  }
+
+  private isObjShallowEqual(
+    obj1: { [index: string]: string },
+    obj2: { [index: string]: string }
+  ) {
+    const keys1 = Object.getOwnPropertyNames(obj1)
+    const keys2 = Object.getOwnPropertyNames(obj2)
+    if (keys1.length !== keys2.length) {
+      return false
+    }
+    const flag = keys1.every((key) => {
+      const type = typeof obj1[key]
+      if (['function', 'array', 'object'].includes(type)) {
+        return type === typeof obj2[key]
+      }
+
+      if (obj1[key] !== obj2[key]) {
+        return false
+      }
+      return true
+    })
+    return flag
+  }
+
+  markdownTableToDataGrid(tableString: string): DataGridTable | void {
+    if (!tableString) {
+      return
+    }
+
+    const lines = tableString.trim().split('\n')
+
+    if (lines.length < 2) {
+      return
+    }
+
+    let columnDef = lines[0].split('|')
+    columnDef.shift()
+    columnDef.pop()
+
+    let column = columnDef.map((el: string) => {
+      return {
+        field: el,
+      }
+    })
+
+    let row = lines.slice(2).map((line: string) => {
+      const rowList = line.split('|')
+      rowList.shift()
+      rowList.pop()
+      const rowItem = rowList.map((value: string, index: number) => {
+        return {
+          [columnDef[index]]: value,
+        }
+      })
+      return Object.assign({}, ...rowItem)
+    })
+
+    return {
+      column,
+      row,
+    }
+  }
+
+  dataGridToMarkdownTable(dataGrid: DataGridTable): string {
+    const { column, row } = dataGrid
+
+    let header = '|'
+    let marker = '|'
+    column.forEach((el) => {
+      header += el.field + '|'
+      marker += '----|'
+    })
+
+    let rowItem = row.map((el) => {
+      const rowData = Object.values(el)
+      const rowLine = '|' + rowData.join('|') + '|'
+      return rowLine
+    })
+
+    let rowString = rowItem.join('\n')
+
+    return header + '\n' + marker + '\n' + rowString
+  }
+
+  async replaceMdFileTable(dataGrid: DataGridTable): Promise<void> {
+    const tableString = this.dataGridToMarkdownTable(dataGrid)
+    const fileContent = await this.app.vault.cachedRead(
+      this.app.workspace.getActiveFile()
+    )
+    const replaceReg = new RegExp(
+      `(?<=tableId:\\s${this.tableId}\\s)[\\w\\W]*(?=\\W+\`\`\`)`
+    )
+    const newFileContent = fileContent.replace(replaceReg, tableString)
+    this.app.vault.modify(this.app.workspace.getActiveFile(), newFileContent)
+  }
+
+  //rowData是选中一个cell时一行的数据，rowIndex是选中该行的位置
+  changeCellValue(
+    dataGrid: DataGridTable,
+    rowData: { [key: string]: string },
+    rowIndex: number
+  ): void {
+    const { column, row } = dataGrid
+
+    let newRow = row.map((el, index) => {
+      if (index === rowIndex) {
+        return rowData
+      } else {
+        return el
+      }
+    })
+
+    this.replaceMdFileTable({ column: column, row: newRow })
+  }
+
+  dragColumn(
+    dataGrid: DataGridTable,
+    colId: string,
+    toIndex: number
+  ): DataGridTable {
+    const { column, row } = dataGrid
+
+    //删除该列
+    const newColumn = column.filter((el: ColDef) => {
+      return el.field != colId
+    })
+    //将该列插入到列的列表中
+    newColumn.splice(toIndex, 0, { field: colId })
+
+    const newRow: Array<{ [key: string]: string }> = []
+    row.map((rowItem: { [index: string]: string }) => {
+      const rowList = Object.entries(rowItem)
+      let fromIndex: number
+      rowList.some((el, index) => {
+        if (el[0] === colId) {
+          fromIndex = index
+          return true
+        }
+      })
+      if (toIndex < fromIndex) {
+        const deletedItem = rowList.splice(fromIndex, 1)
+        rowList.splice(toIndex, 0, deletedItem[0])
+      } else {
+        rowList.splice(toIndex + 1, 0, rowList[fromIndex])
+        rowList.splice(fromIndex, 1)
+      }
+      const rowObj = rowList.map((el: string[]) => {
+        return {
+          [el[0]]: el[1],
+        }
+      })
+      const row = Object.assign({}, ...rowObj)
+      newRow.push(row)
+    })
+
+    this.replaceMdFileTable({ column: newColumn, row: newRow })
+
+    return { column: newColumn, row: newRow }
+  }
+
+  dragRow(
+    dataGrid: DataGridTable,
+    srcRow: { [index: string]: string },
+    toIndex: number
+  ): DataGridTable {
+    const { column, row } = dataGrid
+
+    let newRow = row
+    let fromIndex: number
+    newRow.some((el, index) => {
+      console.log(el, srcRow)
+      if (this.isObjShallowEqual(el, srcRow)) {
+        fromIndex = index
+        return true
+      }
+    })
+    if (fromIndex === undefined) {
+      throw new Error('not found fromIndex!')
+    }
+
+    if (toIndex < fromIndex) {
+      const deletedItem = newRow.splice(fromIndex, 1)
+      newRow.splice(toIndex, 0, deletedItem[0])
+    } else {
+      newRow.splice(toIndex + 1, 0, newRow[fromIndex])
+      newRow.splice(fromIndex, 1)
+    }
+
+    this.replaceMdFileTable({
+      column: column,
+      row: newRow,
+    })
+
+    return {
+      column: column,
+      row: newRow,
+    }
+  }
+
+  addRowBelow(dataGrid: DataGridTable, rowIndex: string): DataGridTable {
+    const { column, row } = dataGrid
+
+    const newRowList = column.map((el: { [key: string]: string }) => {
+      return {
+        [el.field]: '',
+      }
+    })
+    const newRow = Object.assign({}, ...newRowList)
+    row.splice(parseInt(rowIndex) + 1, 0, newRow)
+
+    this.replaceMdFileTable({
+      column: column,
+      row: newRow,
+    })
+
+    return {
+      column: column,
+      row: newRow,
+    }
+  }
+
+  deleteThisRow(dataGrid: DataGridTable, rowIndex: string): DataGridTable  {
+    const { column, row } = dataGrid
+
+    row.splice(parseInt(rowIndex), 1)
+
+    this.replaceMdFileTable({
+      column: column,
+      row: row,
+    })
+    
+    return {
+      column: column,
+      row: row,
+    }
+  }
+}
